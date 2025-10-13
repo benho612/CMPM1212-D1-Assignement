277994d43cdb549534b43282b4cfd470de20bb48
benho612 <151803987+benho612@users.noreply.github.com>
2025-10-13 01:34:39 +0000

    Step 1: Added a button

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+/*import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+`;*/
+
+// Create basic HTML structure
+document.body.innerHTML = `
+  <h1>CMPM 121 Project</h1>
+  <p>Counter: <span id="counter">0</span></p>
+  <button id="increment">Click Me!</button>
 `;
